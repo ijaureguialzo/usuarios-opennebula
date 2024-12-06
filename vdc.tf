ba1845286ee57a6e37bbeaf5e1b786d799d4296f
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-12-06 21:06:36 +0100

    Dar acceso al vdc del usuario

--- vdc.tf
+++ vdc.tf
@@ -4,4 +4,11 @@ resource "opennebula_virtual_data_center" "datacenter" {
   name = "${local.usuarios.nombre}${local.usuarios.separador}${format("%02d", count.index+1)}"
 
   group_ids = [opennebula_group.grupos[count.index].id]
+
+  zones {
+    id = 0
+    host_ids = [0]
+    datastore_ids = [0, 1, 2]
+    vnet_ids = [opennebula_virtual_network.redes[count.index].id]
+  }
 }
