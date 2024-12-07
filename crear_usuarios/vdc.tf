a570cd9da514ea2677fe964ccd2e70075dddfac7
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-12-07 11:40:43 +0100

    Faltaba de actualizar

--- crear_usuarios/vdc.tf
+++ crear_usuarios/vdc.tf
@@ -1,21 +1,17 @@
 resource "opennebula_virtual_data_center" "datacenter" {
-  count = local.usuarios.numero
+  name = var.username
 
-  name = "${local.usuarios.nombre}${local.usuarios.separador}${format("%02d", count.index+1)}"
-
-  group_ids = [opennebula_group.grupos[count.index].id]
+  group_ids = [opennebula_group.grupo.id]
 
   zones {
     id = 0
     host_ids = [0]
     datastore_ids = [0, 1, 2]
-    vnet_ids = [opennebula_virtual_network.redes[count.index].id]
+    vnet_ids = [opennebula_virtual_network.red.id]
   }
 }
 
 resource "null_resource" "borrar_default_vdc_grupo" {
-  count = local.usuarios.numero
-
   connection {
     type = "ssh"
     user = "root"
@@ -25,7 +21,7 @@ resource "null_resource" "borrar_default_vdc_grupo" {
 
   provisioner "remote-exec" {
     inline = [
-      "onevdc delgroup default ${opennebula_group.grupos[count.index].name}"
+      "onevdc delgroup default ${opennebula_group.grupo.name}"
     ]
   }
 }
